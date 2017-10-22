b8ebf709bf9ce01f583aeec6520570504ae3df80
bhardwajaditya <aditya.1998bhardwaj@gmail.com>
2017-10-22 08:04:25 +0530

    FIle added

--- app/src/main/java/com/example/aditya/discovervitcc/menu.java
+++ app/src/main/java/com/example/aditya/discovervitcc/menu.java
@@ -2,6 +2,7 @@ package com.example.aditya.discovervitcc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class menu extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Toolbar t1=(Toolbar)findViewById(R.id.t1);
+        setSupportActionBar(t1);
     }
 }
